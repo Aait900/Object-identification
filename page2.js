708f03d9c8bc6ec9dd6fd11f9c8efe813373283c
Aadit <74043270+Aait900@users.noreply.github.com>
2021-12-28 15:58:36 +0530

    Update page2.js

--- page2.js
+++ page2.js
@@ -1,17 +1,15 @@
 status1=""
 img="";
+objects=[];
 function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML="Status : Detecting Objects";
+    document.getElementById("Status").innerHTML="Status : Detecting Objects";
 }
  
 function preload(){
 img=loadImage('CAR.jpg');
-}
-function draw() {
-    image(img, 0, 0, 640, 420);
 }
 function modelLoaded(){
     console.log("Model Loaded!")
@@ -22,5 +20,26 @@ function gotResult(error, results) {
     if(error) {
         console.log(error);
     }
-    console.log(results);
+    else
+    {console.log(results);
+        objects=results};
+}
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if(status1 !="");
+    {
+        for (i = 0; i < objects.length; i++)
+        { document.getElementById("Status").innerHTML = "Status : Object detected";
+
+        fill('#FF0000');
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke("#FF0000")
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+    }
+       
+    }
+    
 }
